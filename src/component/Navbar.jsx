3ca28b7ecd986e69ba873597b61666522c5e9fa3
islammdsohan603 <islammdsohan603@gmail.com>
2026-03-31 18:38:47 +0600

    navbar toggling bug fixed

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -76,65 +76,63 @@ const Navbar = ({ cart, darkMode, setDarkMode }) => {
       {
         /* Mobile Menu */
         isOpen && (
-          <div className=" lg:hidden  shadow-2xs py-4 w-2xs mx-auto rounded-lg mt-2 right-0 top-12 absolute">
-            <ul className="bg-base-300 shadow-2xl p-10 right-0  fixed z-50 w-fit flex flex-col items-center gap-4 text-sm">
+          <div className="lg:hidden fixed top-16 right-0 z-50 w-64 bg-base-300 shadow-2xl rounded-lg p-6 flex flex-col gap-6">
+            <ul className="flex flex-col items-center gap-4 text-sm">
               <li
                 onClick={() => setIsOpen(false)}
-                className="text-lg text-neutral-500 font-semibold rounded-xl px-2 py-1 cursor-pointer hover:bg-linear-to-l from-[#F875] to-[#6287]  duration-300 transition-transform"
+                className="menu-item দে
+              w-full p-2 bg-base-100 hover:bg-base-300 duration-300 text-lg rounded-2xl text-black font-semibold cursor-pointer"
               >
                 <a href="#products">Products</a>
               </li>
               <li
                 onClick={() => setIsOpen(false)}
-                className="text-lg text-neutral-500 font-semibold rounded-xl px-2 py-1 cursor-pointer hover:bg-linear-to-l from-[#F875] to-[#6287]  duration-300 transition-transform"
+                className="menu-item দে
+              w-full p-2 bg-base-100 hover:bg-base-300 duration-300 text-lg rounded-2xl text-black font-semibold cursor-pointer"
               >
                 <a href="#features">Features</a>
               </li>
               <li
                 onClick={() => setIsOpen(false)}
-                className="text-lg text-neutral-500 font-semibold rounded-3xl px-2 py-1 cursor-pointer hover:bg-linear-to-l from-[#F875] to-[#6287]  duration-300 transition-transform"
+                className="menu-item দে
+              w-full p-2 bg-base-100 hover:bg-base-300 duration-300 text-lg rounded-2xl text-black font-semibold cursor-pointer"
               >
                 <a href="#pricing">Pricing</a>
               </li>
               <li
                 onClick={() => setIsOpen(false)}
-                className="text-lg text-neutral-500 font-semibold rounded-xl px-2 py-1 cursor-pointer hover:bg-linear-to-l from-[#F875] to-[#6287]  duration-300 transition-transform"
+                className="menu-item দে
+              w-full p-2 bg-base-100 hover:bg-base-300 duration-300 text-lg rounded-2xl text-black font-semibold cursor-pointer"
               >
                 <a href="#testimonials">Testimonials</a>
               </li>
               <li
                 onClick={() => setIsOpen(false)}
-                className="text-lg text-neutral-500 font-semibold rounded-xl px-2 py-1 cursor-pointer hover:bg-linear-to-l from-[#F875] to-[#6287] duration-300 transition-transform"
+                className="menu-item দে
+              w-full p-2 bg-base-100 hover:bg-base-300 duration-300 text-lg rounded-2xl text-black font-semibold cursor-pointer"
               >
                 <a href="#faq">FAQ</a>
               </li>
             </ul>
 
-            <div className="md:hidden flex flex-col items-center  gap-4">
-              <div onClick={() => setIsOpen(false)} className=" relative">
-                <ShoppingCart className="w-6 h-6 mt-5 text-[#4F39F6] hover:scale-110 transition-transform duration-300 cursor-pointer" />
-                <span className="absolute mt-5 -top-2 -right-2 bg-[#4F39F6] text-white text-xs w-5 h-5 rounded-full flex items-center justify-center font-bold">
+            <div className="flex flex-col items-center gap-4">
+              <div className="relative">
+                <ShoppingCart className="w-6 h-6 text-[#4F39F6]" />
+                <span className="absolute -top-2 -right-2 bg-[#4F39F6] text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
                   {cart.length}
                 </span>
               </div>
 
-              <button
-                onClick={() => setIsOpen(false)}
-                className="px-4 py-2 text-sm sm:text-base rounded-full  text-[#4F39F6] bg-transparent cursor-pointer font-semibold shadow-2xs hover:scale-110 transition-transform duration-300"
-              >
+              <button className="btn btn-login w-full rounded-2xl shadow-2xl cursor-pointer">
                 Login
               </button>
-              <button
-                onClick={() => setIsOpen(false)}
-                className="px-4 py-2 text-sm sm:text-base cursor-pointer font-semibold border border-[#4F39F6] text-white bg-linear-to-r from-[#4F39F6] to-[#627382] rounded-full"
-              >
+              <button className="btn btn-primary w-full rounded-2xl cursor-pointer hover:shadow-2xl duration-300">
                 Get Started
               </button>
 
-              {/* DarkMode */}
               <div
                 onClick={handleDarkMode}
-                className=" w-full h-10 object-contain rounded-full bg-base-200 flex md:hidden items-center justify-center cursor-pointer hover:scale-105 duration-300 hover:bg-base-300 text-black"
+                className="w-full h-10 flex items-center justify-center bg-base-200 rounded-full cursor-pointer"
               >
                 {darkMode ? <Sun /> : <MoonStar />}
               </div>
